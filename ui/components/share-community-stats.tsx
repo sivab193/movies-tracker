4bcad6e3bb686eb503c7146b094ff98269072cad
Sivaganesh B <67482763+sivab193@users.noreply.github.com>
2026-08-25 18:58:45 +0530

    chore: update MediaVerse domain in ui/components/share-community-stats.tsx

--- ui/components/share-community-stats.tsx
+++ ui/components/share-community-stats.tsx
@@ -27,7 +27,7 @@ export interface CommunityStats {
     totalTheaters: number
 }
 
-const SITE = "mv.siv19.dev"
+const SITE = "media-verse.in"
 const INSTA = "@media.verse.tv"
 
 function roundRect(
